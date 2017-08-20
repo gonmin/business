e09bdd1f5d5e379b86cb2646c1b640aa237fe300
jAVASCRIPT <2457907221@qq.com>
2017-08-20 21:05:05 +0800

    revise

---
+++ src/page/index/index.js
@@ -0,0 +1,15 @@
+// cats = require('./cats.js');
+
+// console.log(cats);
+
+var $ = require('jquery');
+
+console.log('index');
+
+$('body').html('HELLO INDEX');
+
+
+(function() {
+	console.log('index');
+
+})()
