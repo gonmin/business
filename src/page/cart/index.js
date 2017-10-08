73fa24d6ab78dba557f6d442b9d805f893eefd52
gonmin <860282341.com>
2017-10-08 20:54:47 +0800

    order-confirm

--- src/page/cart/index.js
+++ src/page/cart/index.js
@@ -148,7 +148,7 @@ var page = {
 		// 提交购物车
 		$(document).on('click', '.btn-submit', function () {
 			if (_this.data.cartInfo && _this.data.cartInfo.cartTotalPrice > 0) {
-				window.location.href = './confirm.html';
+				window.location.href = './order-confirm.html';
 			} else {
 				_mm.errorTips('请选择商品后再提交');
 
