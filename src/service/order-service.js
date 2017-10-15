2d0075ef4dc137722d8ceea079d1c71b410c0533
gonmin <860282341.com>
2017-10-15 10:50:36 +0800

    order

--- src/service/order-service.js
+++ src/service/order-service.js
@@ -9,6 +9,46 @@ var  _order = {
 			error: reject
 		})
 	},
+	// 获取订单列表
+	getOrderList: function (listParam, resolve, reject) {
+		_mm.request({
+			url: _mm.getServerUrl('/order/list.do'),
+			data: listParam,
+			success: resolve,
+			error: reject
+		})
+	},
+	// 提交订单
+	createOrder: function (orderInfo, resolve, reject) {
+		_mm.request({
+			url: _mm.getServerUrl('/order/create.do'),
+			data: orderInfo,
+			success: resolve,
+			error: reject
+		})
+	},
+	// 获取订单详情
+	getOrderDetail: function (orderNum, resolve, reject) {
+		_mm.request({
+			url: _mm.getServerUrl('/order/detail.do'),
+			data: {
+				orderNo: orderNum	
+			},
+			success: resolve,
+			error: reject
+		})
+	},
+	// 取消订单
+	cancelOrder: function (orderNum, resolve, reject) {
+		_mm.request({
+			url: _mm.getServerUrl('/order/cancel.do'),
+			data: {
+				orderNo: orderNum	
+			},
+			success: resolve,
+			error: reject
+		})
+	},
 	// 获取商品列表
 	getProductDetail: function (productId, resolve, reject) {
 		_mm.request({
